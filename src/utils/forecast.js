c84c9fde3cd22f7290ea72c9140df56f712b8007
mohankm1 <mohankmshetty@gmail.com>
2020-12-27 16:07:28 -0600

    added forecast humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,10 @@ request({url, json: true}, (error, {body})=>{
     } else if (body.error){
         callback('Unable to find location', undefined)
     } else {
-        callback(undefined, body.current.weather_descriptions[0]+ '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' +body.current.feelslike+ ' degrees out.')
+        callback(undefined, body.current.weather_descriptions[0]+ 
+            '. It is currently ' + body.current.temperature + 
+            ' degrees out. It feels like ' +body.current.feelslike+ ' degrees out.'+
+            ' Humidity is '+ body.current.humidity +'%.')
     }
 })
 
